32257f5aba2feeac2a903a5b563a3753d9065093
micaela.zampone <micaela.zampone@jedify.io>
2023-03-09 09:34:55 -0300

    sass

---
+++ src/components/Barra.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import '../styles/barra/barra.scss';
+// import 'src/styles/barra/barra.scss';
+
+function Barra() {
+
+
+  return (
+    <div className='box'
+    style={{color:"black",
+    marginTop: '0px',
+    height: '3.5rem',
+    width: '100%',
+    backgroundColor: 'yellow',
+    display:"flex",
+    justifyContent: 'flex-start'
+
+    }}
+    >
+        hola soy giuli y soy una banana
+    </div>
+  )
+}
+
+export default Barra
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Barra from '../components/Barra'
+
+function Home() {
+  return (
+    <div 
+    // style={{height: 'auto'
+    // }}
+    >
+      <Barra/>
+    </div>
+  )
+}
+
+export default Home
